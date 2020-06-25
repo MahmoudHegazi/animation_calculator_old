1821e585bf54329e77677c2ad3c96ea73ee718db
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-06-25 14:31:10 +0200

    Update calculator_An.js

--- calculator_An.js
+++ calculator_An.js
@@ -8,7 +8,13 @@ const mybject = {
 	"result": 0,
 	add : () => {
 		//function add
-		mybject.result = Number(mybject.numbers[0]) + Number(mybject.numbers[1]);	
+		let inputs = mybject.numbers;
+		var result = 0;
+        var i;
+		for (i = 0; i < inputs.length; i++) {
+          result += inputs[i];
+        }
+		mybject.result = result;
 	},
 	subs : () => {
 		// function substrict
@@ -34,6 +40,71 @@ const mybject = {
 		// remove last index of array array.pop();
 		mybject.numbers.pop();
 	},
+	insert : function(x){
+		mybject.numbers.push();
+	}
 	
 }
 
+// start
+
+this.num_.addEventListener("click", num0.bind(this));
+function num0() {
+
+}
+
+this.num_.addEventListener("click", num1.bind(this));
+function num1() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num2.bind(this));
+function num2() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num3.bind(this));
+function num3() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num4.bind(this));
+function num4() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num5.bind(this));
+function num5() {
+	if () {
+
+	}
+}
+
+this.num_.addEventListener("click", num6.bind(this));
+function num6() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num7.bind(this));
+function num7() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num8.bind(this));
+function num8() {
+	if () {
+
+	}
+}
+this.num_.addEventListener("click", num9.bind(this));
+function num9() {
+	if () {
+
+	}
+}
