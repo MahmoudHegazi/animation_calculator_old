94f637b3d80c1d9d5e22f3079a13e8df121a72a3
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-06-25 15:15:40 +0200

    Update calculator_An.js

--- calculator_An.js
+++ calculator_An.js
@@ -1,9 +1,14 @@
-var result = this.screen;
-
-
 
 const mybject = {
 	"numbers" : [],
+	"num1" : "",
+    "num2" : "",
+	"num3" : "",
+	"num4" : "",
+	"num5" : "",
+	"num6" : "",
+	"num7" : "",
+	"num8" : "",
 	"ope" : "",
 	"result": 0,
 	add : () => {
@@ -26,7 +31,13 @@ const mybject = {
 	},
 	multi : () => {
 		// function multiplay
-		mybject.result = Number(mybject.numbers[0]) * Number(mybject.numbers[1]);
+		let inputs = mybject.numbers;
+		var result = 1;
+        var i;
+		for (i = 0; i < inputs.length; i++) {
+          result *= inputs[i];
+        }
+		mybject.result = result;
 	},
 	mod : () => {
 		// modulu function
@@ -48,63 +59,77 @@ const mybject = {
 
 // start
 
-this.num_.addEventListener("click", num0.bind(this));
+this.num_0.addEventListener("click", num0.bind(this));
 function num0() {
-
+   mybject.numbers.push(0);
+   this.screen.text = "0";
 }
 
-this.num_.addEventListener("click", num1.bind(this));
+this.num_1.addEventListener("click", num1.bind(this));
 function num1() {
-	if () {
-
-	}
+   mybject.numbers.push(1);
+   this.screen.text = "1";
 }
-this.num_.addEventListener("click", num2.bind(this));
+this.num_2.addEventListener("click", num2.bind(this));
 function num2() {
-	if () {
-
-	}
+   mybject.numbers.push(2);
+   this.screen.text = "2";
 }
-this.num_.addEventListener("click", num3.bind(this));
+this.num_3.addEventListener("click", num3.bind(this));
 function num3() {
-	if () {
-
-	}
+   mybject.numbers.push(3);
+   this.screen.text = "3";
 }
-this.num_.addEventListener("click", num4.bind(this));
+this.num_4.addEventListener("click", num4.bind(this));
 function num4() {
-	if () {
-
-	}
+   mybject.numbers.push(4);
+   this.screen.text = "4";
 }
-this.num_.addEventListener("click", num5.bind(this));
+this.num_5.addEventListener("click", num5.bind(this));
 function num5() {
-	if () {
-
-	}
+   mybject.numbers.push(5);
+   this.screen.text = "5";
 }
 
-this.num_.addEventListener("click", num6.bind(this));
+this.num_6.addEventListener("click", num6.bind(this));
 function num6() {
-	if () {
-
-	}
+   mybject.numbers.push(6);
+   this.screen.text = "6";
 }
-this.num_.addEventListener("click", num7.bind(this));
+this.num_7.addEventListener("click", num7.bind(this));
 function num7() {
-	if () {
-
-	}
+   mybject.numbers.push(7);
+   this.screen.text = "7";
 }
-this.num_.addEventListener("click", num8.bind(this));
+this.num_8.addEventListener("click", num8.bind(this));
 function num8() {
-	if () {
-
-	}
+   mybject.numbers.push(8);
+   this.screen.text = "8";
 }
-this.num_.addEventListener("click", num9.bind(this));
+this.num_9.addEventListener("click", num9.bind(this));
 function num9() {
-	if () {
-
-	}
+   mybject.numbers.push(9);
+   this.screen.text = "9";
+}
+this.ope_add.addEventListener("click", adding.bind(this));
+function adding() {
+   mybject.ope = "+";
+   this.screen.text = "+";
+}
+this.ope_multi.addEventListener("click", multiplaing.bind(this));
+function multiplaing() {
+   mybject.ope = "*";
+   this.screen.text = "*";
+}
+this.result.addEventListener("click", final_result.bind(this));
+function final_result() {
+  	
+  if (mybject.ope == "+") {
+	  mybject.add();
+	  this.screen.text = mybject.result;
+  }
+  if (mybject.ope == "*") {
+	  mybject.multi();
+	  this.screen.text = mybject.result;
+  }
 }
